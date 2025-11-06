4aba59013f26d82d03e5d529f11e4a3df148edfa
Emelie Ilola <emelie.ilola@outlook.com>
2025-11-06 09:44:19 +0100

    Even more code

--- src/exercise11/HelloWorld.java
+++ src/exercise11/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Cats!");
         System.out.println("Hello Dogs!");
-
+        System.out.println("Even more code");
         System.out.println("Hello World!! How are you?");
 
     }
