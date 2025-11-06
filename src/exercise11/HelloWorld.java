e663d4bc6634876b0ed2f629756079e486bca625
emelieIlola <emelie.ilola@outlook.com>
2025-11-06 09:40:27 +0100

    Update HelloWorld.java
    Cats and dogs

--- src/exercise11/HelloWorld.java
+++ src/exercise11/HelloWorld.java
@@ -5,8 +5,8 @@ import javax.xml.transform.Source;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Cat");
-        System.out.println("Hello Dog");
+        System.out.println("Hello Cats!");
+        System.out.println("Hello Dogs!");
 
         System.out.println("Hello World!!");
 
